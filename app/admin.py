864775d06c59a6c660dc64d395bbe9a42322462a
Kosmos_667 <sobinin2000@mail.ru>
2024-01-12 16:45:13 +0300

    first commit

---
+++ app/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.contrib.auth.models import Group
+
+from .models import Product, Customer, Cart, Wishlist
+
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'discounted_price', 'category', 'product_image']
+
+
+@admin.register(Customer)
+class Admin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'locality', 'city', 'state', 'zipcode']
+
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'products', 'quantity']
+
+    def products(self, obj):
+        link = reverse('admin:app_product_change', args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.product.title)
+
+
+@admin.register(Wishlist)
+class WishlistModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'products']
+
+    def products(self, obj):
+        link = reverse('admin:app_product_change', args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.product.title)
+
+
+admin.site.unregister(Group)
+
+
+
+
+
